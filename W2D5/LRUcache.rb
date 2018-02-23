7ae71acf978ced757bc27e13f0eb9094e4ff9717
Renel <l2enel@hotmail.com>
2018-02-23 07:25:47 -0800

    Complete bad LRUcache

---
+++ W2D5/LRUcache.rb
@@ -0,0 +1,44 @@
+class LRUCache
+
+  attr_reader :max_size
+  attr_accessor :cache
+
+    def initialize(max_size)
+      @cache = []
+      @max_size = max_size
+    end
+
+    def count
+      # returns number of elements currently in cache
+      cache.length
+    end
+
+    def add(el)
+      # adds element to cache according to LRU principle
+        if self.count >= max_size
+          if cache.include?(el)
+            cache.delete(el)
+            cache << el
+          else
+            cache.shift
+            cache << el
+          end
+        else
+          if cache.include?(el)
+            cache.delete(el)
+            cache << el
+          else
+            cache << el
+          end
+        end
+    end
+
+    def show
+      # shows the items in the cache, with the LRU item first
+      cache
+    end
+
+    private
+    # helper methods go here!
+
+  end
